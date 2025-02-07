876e87e1c8e26664d54e1df9f9a0039219d4644c
arkiver <arkiver@protonmail.com>
2025-02-07 20:16:26 +0100

    Version 20250207.02. Fix string.

--- usgovernment.lua
+++ usgovernment.lua
@@ -731,7 +731,7 @@ wget.callbacks.write_to_warc = function(url, http_stat)
       "auth",
       "account",
       "oauth",
-      "authori[sz]e
+      "authori[sz]e"
     }) do
       if string.match(newloc, "[^0-9a-zA-Z]" .. s .. "[^0-9a-zA-Z]") then
         io.stdout:write("Bad redirect to " .. newloc .. ".\n")
